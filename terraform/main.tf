405e8d61b0fda86adebaffcfa447ae92bbad27a9
hinula <gemalmazhira@gmail.com>
2026-06-17 01:10:21 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
-  # DNS ve *.localhost yönlendirme sorunlarını çözen kritik parametre
+  
   s3_use_path_style           = true
 
   endpoints {
